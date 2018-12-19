d4ff94b4fe41c1bf1dfb0cf761a9a0307e1fc522
Ferry <allan.ferry@orange.fr>
2018-12-19 16:11:03 +0100

    comme ça ? ça marche?

--- index.php
+++ index.php
@@ -10,23 +10,7 @@
         <h1>Mon super blog !</h1>
         <p>Derniers billets du blog :</p>
  
-<?php
-// Connexion à la base de données
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=miniblog;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
 
-// On récupère les 5 derniers billets
-$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
-while ($donnees = $req->fetch())
-{
-?>
 <div class="news">
     <h3>
         <?php echo htmlspecialchars($donnees['titre']); ?>
