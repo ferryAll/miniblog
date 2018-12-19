f905840322ecc3f399858c96b831dbc7d5fdc5bf
Ferry <allan.ferry@orange.fr>
2018-12-19 16:15:11 +0100

    retour à la normale Grrrrrrrrrrrrrrrrrrrrrr

--- commentaires.php
+++ commentaires.php
@@ -10,6 +10,22 @@
         <h1>Mon super blog !</h1>
         <p><a href="index.php">Retour à la liste des billets</a></p>
  
+<?php
+// Connexion à la base de données
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=miniblog;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+// Récupération du billet
+$req = $bdd->prepare('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets WHERE id = ?');
+$req->execute(array($_GET['billet']));
+$donnees = $req->fetch();
+?>
 
 <div class="news">
     <h3>
