3e6ee8464cb607879f09e8405ad4255134f39e52
Ferry <allan.ferry@orange.fr>
2018-12-20 16:08:14 +0100

    ;b c

--- indexRepository.php
+++ indexRepository.php
@@ -1,39 +1,36 @@
-
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
         <title>Mon blog</title>
-	<link href="style.css" rel="stylesheet" /> 
+        <link href="style.css" rel="stylesheet" /> 
     </head>
         
     <body>
         <h1>Mon super blog !</h1>
-        <p>Derniers billets du blog :</p>
- 
-<?php
-while ($donnees = $req->fetch())
-{
-?>
-<div class="news">
-    <h3>
-        <?php echo htmlspecialchars($donnees['titre']); ?>
-        <em>le <?php echo $donnees['date_creation_fr']; ?></em>
-    </h3>
-    
-    <p>
-    <?php
-    // On affiche le contenu du billet
-    echo nl2br(htmlspecialchars($donnees['contenu']));
-    ?>
-    <br />
-    <em><a href="#">Commentaires</a></em>
-    </p>
-</div>
-<?php
-} // Fin de la boucle des billets
-$req->closeCursor();
-?>
-</body>
+        <p><a href="index.php">Retour à la liste des billets</a></p>
+
+        <div class="news">
+            <h3>
+                <?= htmlspecialchars($post['title']) ?>
+                <em>le <?= $post['creation_date_fr'] ?></em>
+            </h3>
+            
+            <p>
+                <?= nl2br(htmlspecialchars($post['content'])) ?>
+            </p>
+        </div>
+
+        <h2>Commentaires</h2>
+
+        <?php
+        while ($comment = $comments->fetch())
+        {
+        ?>
+            <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
+            <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+        <?php
+        }
+        ?>
+    </body>
 </html>
