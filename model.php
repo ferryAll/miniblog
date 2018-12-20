e65b86dbf8f907085c83c2c626fdd730a20adf4b
Ferry <allan.ferry@orange.fr>
2018-12-20 15:29:53 +0100

    au point Mortel de peter une durite sévère

--- model.php
+++ model.php
@@ -1,17 +1,47 @@
 <?php
 function getBillets()
 {
+    $db = dbConnect();
+    $req = $db->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+   
+    return $req;
+}
+
+function getPosts()
+{
+    // ... deja écrite
+}
+
+function getPost($postId)
+{
+    $db = dbConnect();
+    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+    $req->execute(array($postId));
+    $post = $req->fetch();
+}
+
+
+
+function getComments($postId)
+{
+    $db = dbConnect();
+
+    $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+    $comments->execute(array($postId));
+
+    return $comments;
+}
+
+//une nouvelle fonction
+
+function dbConnect()
+{
     try
     {
-        $bdd = new PDO('mysql:host=localhost;dbname=miniblog;charset=utf8','root','');
+        $db = new PDO('mysql:host=localhost;dbname=miniblog;charset=utf8','root','');
     }
     catch(Exception $e)
     {
         die('Erreur : '.$e->getMessage());
     }
-
-
-    $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
-    return $req;
 }
---
+++ post.php
@@ -0,0 +1,13 @@
+<?php
+require('model.php');
+
+if(isset($_GET['id']) && $_GET['id'] >à) {
+    $post = getPost([$_GEET['id']);
+    $comments = getComments($_GET['id']);
+    require('indexRepository');
+}
+
+else
+{
+    echo 'Erreur : aucun identifiant de billet envoyé';
+}
